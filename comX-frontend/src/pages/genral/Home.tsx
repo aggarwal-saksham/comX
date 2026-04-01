266a52c4be013ade8558e6797b28a32e5896d575
Saksham Aggarwal <saksham080305@gmail.com>
2026-04-02 00:09:36 +0530

    update homepage title

--- comX-frontend/src/pages/genral/Home.tsx
+++ comX-frontend/src/pages/genral/Home.tsx
@@ -28,7 +28,7 @@ export default function HomePage() {
         <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-0">
           <div>
             <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-              Project for Software Engineering
+              A Unified Workspace for Software Teams
               <br /> <Cover>COM-X</Cover>
             </h1>
           </div>
